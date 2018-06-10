592ba4353975c7ffb1c6fb8682a3c32770736e3c
Justin Meskan <justinmeskan@gmail.com>
2018-06-09 17:47:59 -0700

    applied background to days on calendar that dont have availabilies.

--- client/src/calendarDay.jsx
+++ client/src/calendarDay.jsx
@@ -11,13 +11,25 @@ const Span = styled.span`
   display: flex;
   flex-flow:column;
   font-size: 12px;
+  background-image:url(${BG});
+  background-size: 300% 300%;
+  opacity: .3;
+`;
+
+const Else = styled.span`
+  height:120%;
+  width:13%;
+  border:1px solid black;
+  text-align:center;
+  display: flex;
+  flex-flow:column;
+  font-size: 12px;
 `;
 
 const Div = styled.div`
   padding-top: 2px;
   font-weight: bolder;
   font-size: 14px;
-  background-image:url(${BG});
 `;
 
 class CalendarDay extends React.Component {
@@ -27,7 +39,8 @@ class CalendarDay extends React.Component {
   }
 
   render() {
-    return (
+    if( this.props.day && this.props.cost === undefined || this.props.cost === '' ){
+      return (
         <Span>
           <Div>
             {  this.props.day }
@@ -35,9 +48,20 @@ class CalendarDay extends React.Component {
           <div>
             {  this.props.cost === undefined || this.props.cost === '' ? '' : '$'+this.props.cost  }
           </div>
-          
         </Span>
-    )
+      )
+    }else{
+      return (
+        <Else>
+          <Div>
+            {  this.props.day }
+          </Div>
+          <div>
+            {  this.props.cost === undefined || this.props.cost === '' ? '' : '$'+this.props.cost  }
+          </div>
+        </Else>
+      )
+    }
   }
 };
 
