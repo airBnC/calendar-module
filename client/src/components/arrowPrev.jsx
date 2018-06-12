dd693ad3a7e10f5323b2f04868ab738b48c2fc72
Justin Meskan <justinmeskan@gmail.com>
2018-06-11 19:12:30 -0700

    ecerything works components folder setup, now i'm going to create my test folder

--- client/src/components/arrowPrev.jsx
+++ client/src/components/arrowPrev.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
 import styled from 'styled-components';
-import arrow from '../../img/arrow.png';
+import arrow from '../../../img/arrow.png';
 
 const Div = styled.div`
   &:hover {
