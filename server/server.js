4f2c6650d632ca06ea5d5aa6f1d4c2bd718c5aa9
justin meskan <justinmeskan@justins-MacBook-Pro-2.local>
2018-06-15 11:09:49 -0700

    fix a little naming bug should work let me know.

--- server/server.js
+++ server/server.js
@@ -1,65 +1,12 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/rooms/:id',express.static(__dirname + '/../client/'));
+app.use('/rooms/:id',express.static(path.join(__dirname, '/../client/')));
 require('./routes.js')(app);
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
